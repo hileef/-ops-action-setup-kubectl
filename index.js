00d3f6d362231045d048fc49cb8169732623e5f5
Eliot Hautefeuille <ehautefeuille@scortex.io>
2020-05-27 16:35:14 +0200

    initial attempt

---
+++ index.js
@@ -0,0 +1,63 @@
+const http = require("http");
+const fs = require("fs");
+const util = require("util");
+
+const core = require('@actions/core');
+// const github = require('@actions/github');
+
+function download(url, dest) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest, { flags: "wx" });
+
+        const request = http.get(url, response => {
+            if (response.statusCode === 200) {
+                response.pipe(file);
+            } else {
+                file.close();
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
+            }
+        });
+
+        request.on("error", err => {
+            file.close();
+            fs.unlink(dest, () => {}); // Delete temp file
+            reject(err.message);
+        });
+
+        file.on("finish", () => {
+            resolve();
+        });
+
+        file.on("error", err => {
+            file.close();
+
+            if (err.code === "EEXIST") {
+                reject("File already exists");
+            } else {
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(err.message);
+            }
+        });
+    });
+}
+
+try {
+    const kubeVersion = core.getInput('version');
+    console.log(`Selected kubectl version is : ${kubeVersion}`);
+
+    const kubeURL = "https://storage.googleapis.com/kubernetes-release/release/%s/bin/linux/amd64/kubectl"
+    const destPath = "/usr/local/bin/kubectl"
+    const finalURL = util.format(kubeURL, kubeVersion)
+    const p = download(finalURL, destPath)
+    p.then(function (_) {
+        fs.chmodSync(destPath, '777');
+        console.log(`Downloaded kubectl version ${kubeVersion} !`);
+
+    }).catch(err => {
+        core.setFailed(err.message);
+    })
+
+} catch (error) {
+    core.setFailed(error.message);
+}
